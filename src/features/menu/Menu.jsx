cd9cf4253465e6de6ea84253102037e227fa6329
MBANS8A1 <seanmbng@aol.com>
2026-01-04 00:03:12 +0000

    The async loader function returns the menu data via the getMenu function. This will be provided to the "/menu" page.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,7 +1,12 @@
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
   return <h1>Menu</h1>;
 }
 
-function loader() {}
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 
 export default Menu;
