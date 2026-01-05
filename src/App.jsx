c102d060f8041766e08359296e524898ea45e936
MBANS8A1 <seanmbng@aol.com>
2026-01-05 23:39:30 +0000

    I added the errorElement property and the Error component as its value to the nested route object for parent layout. This is so the error is shown in the layout and does not take up the entire viewport of the screen.

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
