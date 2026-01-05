6c5f4e4af3266ff5482d97d0fa50b195f711cd37
MBANS8A1 <seanmbng@aol.com>
2026-01-05 15:43:57 +0000

    I added the onSubmit prop to the <form> with handleSubmit function. Now, when an orderId is typed in the input bar, which is nested in the Header component, the URL will change to "http://localhost:5173/order/:orderId". This is done with the help of the useNavigate() hook from react-router-dom.

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
   const [orderQuery, setOrderQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!orderQuery) return;
+    navigate(`/order/${orderQuery}`);
+    setOrderQuery("");
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         placeholder="Search the order #"
         onChange={(e) => setOrderQuery(e.target.value)}
