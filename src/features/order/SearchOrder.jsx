e1a571b3d50573827ba9fc098886905f25322f3c
MBANS8A1 <seanmbng@aol.com>
2026-01-14 22:03:08 +0000

    Regarding the "focus:" state of the SearchOrder input field, I added a yellow ring with no outline on focus. To make the ring appearance on focus more subtle I added a ring opacity of ring-opacity-50.

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,7 +17,7 @@ function SearchOrder() {
         placeholder="Search the order #"
         onChange={(e) => setOrderQuery(e.target.value)}
         value={orderQuery}
-        className="placeholder: w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-72 sm:w-64"
+        className="focus:ring-opacity-50 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-72 focus:ring focus:ring-yellow-500 focus:outline-none sm:w-64"
       />
     </form>
   );
