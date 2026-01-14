b5b100f3ed9c4602fa4660638011fb2ef51205d7
MBANS8A1 <seanmbng@aol.com>
2026-01-14 13:13:16 +0000

    To the button used for creating new orders, I added additional Tailwind styling related to transitions and element states. I added a coloured focus ring, which is offset from the button element. When the button is hovered over, there will be a 300ms transition to a lighter yellow background. When the button is clicked and disabled via boolean state change of isSubmitting, the cursor will be removed and be replaced with a not allowed icon.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -84,7 +84,7 @@ function CreateOrder() {
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button
             disabled={isSubmitting}
-            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase hover:bg-yellow-300"
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed"
           >
             {isSubmitting ? 'Placing order....' : 'Order now'}
           </button>
