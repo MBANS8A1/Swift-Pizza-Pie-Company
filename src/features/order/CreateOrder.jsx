c49054a6b9f412f181fe1976db251df1f014eafb
MBANS8A1 <seanmbng@aol.com>
2026-01-15 21:05:11 +0000

    To make my Tailwind CSS reusable, I created an ".input" class within the Tailwind @layer for components and added the className code after the @apply directive. Now the ".input" class can be reused in the components.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -63,12 +63,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-400 focus:outline-none md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
