9e3a712fb95c801b8f38280353b0bdde7c7931d4
MBANS8A1 <seanmbng@aol.com>
2026-01-16 23:42:27 +0000

    I extracted the Tailwind classes used for the Link JSX in the LinkButton component into a variable called linkButtonClass. I used this variable to provide the Tailwind classes to the button element when prop is "-1" and to the Link when the prop is not "-1" (goes according to the principle of DRY).

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -2,15 +2,18 @@ import { Link, useNavigate } from 'react-router-dom';
 
 function LinkButton({ children, to }) {
   const navigate = useNavigate();
+  const linkButtonClass =
+    'text-sm text-blue-500 hover:text-blue-600 hover:underline';
 
   if (to === '-1') {
-    return <button onClick={() => navigate(-1)}>{children}</button>;
+    return (
+      <button className={linkButtonClass} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
   }
   return (
-    <Link
-      to={to}
-      className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
-    >
+    <Link to={to} className={linkButtonClass}>
       {children}
     </Link>
   );
