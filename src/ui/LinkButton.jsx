fc5f2feeca9f7ce3868d06e6b27b649c0faf514e
MBANS8A1 <seanmbng@aol.com>
2026-01-16 16:29:19 +0000

    The to (endpoint) prop of the Link returned from the LinkButtom component is set dynamically as is the children content.

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function LinkButton({ children, to }) {
-  return <div></div>;
+  return (
+    <Link
+      to={to}
+      className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+    >
+      {children}
+    </Link>
+  );
 }
 
 export default LinkButton;
