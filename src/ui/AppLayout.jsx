53de61a649eca7e494416882d44a8f13338783fb
MBANS8A1 <seanmbng@aol.com>
2026-01-17 23:48:41 +0000

    I restored line 11 in the AppLayout.jsx file, so the LoadingSpinner component instance will only be rendered on the UI if isLoading is true.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,8 +8,7 @@ function AppLayout() {
   const isLoading = navigation.state === 'loading';
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {/* {isLoading && <LoadingSpinner />} */}
-      <LoadingSpinner />
+      {isLoading && <LoadingSpinner />}
       <Header />
       <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl">
