a72684c7db13c762c71854c419833d13b4bad8d9
MBANS8A1 <seanmbng@aol.com>
2026-01-17 18:54:27 +0000

    In order to make the LoadingSpinner container (<div>) cover the entire viewport of the UI when in operation, I used inset-0 (sets top, bottom, left and right to 0). Also I added a small pixelated blur with a slate background, which has 20% opacity.

--- src/ui/LoadingSpinner.jsx
+++ src/ui/LoadingSpinner.jsx
@@ -1,5 +1,9 @@
 function LoadingSpinner() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default LoadingSpinner;
