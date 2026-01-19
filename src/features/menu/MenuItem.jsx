49b581d024cd687431e9a08468886bb20ec8ea0e
MBANS8A1 <seanmbng@aol.com>
2026-01-19 12:34:15 +0000

    I added some padding between each MenuItem list element and the page with Tailwind's py-2.

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,9 +4,9 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li className="flex gap-4">
+    <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className="h-24" />
-      <div>
+      <div className="">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
         <div>
