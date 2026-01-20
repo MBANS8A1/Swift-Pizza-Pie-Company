283147f8abdad7981e8808d74c6f0e800583a465
MBANS8A1 <seanmbng@aol.com>
2026-01-20 16:58:48 +0000

    In the Button component, I made a styles object, which has a property of primary; primary's value is the base Tailwind styling variable plus the padding and padding specific for the width of the small breakpoint and larger. I plan on removing buttonClass as it is redundant.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed ';
   const buttonClass =
     'inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4';
   if (to)
@@ -9,6 +11,10 @@ function Button({ children, disabled, to }) {
         {children}
       </Link>
     );
+
+  const styles = {
+    primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
+  };
 
   return (
     <button disabled={disabled} className={buttonClass}>
