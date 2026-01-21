9b4dd0f2feee852f7894efb362eb320ae1edfacd
MBANS8A1 <seanmbng@aol.com>
2026-01-21 23:41:26 +0000

    At very large viewport widths the is large space between the totalPrice and delete Button due to justify-between; it does not look good aesthetically.  So, I made the flex behavior only occur at the small breakpoint by applying the flex settings to the <li> MenuItem. and applying a gap to the container <div> of totalPrice and the delete Button. This will make the spacing more consistent between the flex items.

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -5,11 +5,11 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="py-3 sm:flex sm:items-center sm:justify-center">
       <p className="mb-1">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
       </div>
