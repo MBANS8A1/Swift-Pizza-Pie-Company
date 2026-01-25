58c3d864ca12166c08df147327fc056fb1376fdb
MBANS8A1 <seanmbng@aol.com>
2026-01-25 15:57:12 +0000

    For the paragraph elements in the <div> container related to estimatedDelivery, I formatted the test using Tailwind classes. For the 1st one I made the text font-weight 500 and for the 2nd one, I made the text smaller and coloured it a stone shade.

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -42,12 +42,14 @@ function Order() {
       </div>
 
       <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
-        <p>
+        <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : 'Order should have arrived'}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-sm text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
       <div>
