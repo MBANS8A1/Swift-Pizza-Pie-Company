c975131d0ed342a2296882649daf25bb35cdf7de
MBANS8A1 <seanmbng@aol.com>
2026-01-25 20:18:26 +0000

    For the <div> container with pricing information for the order, I added vertical spacing, padding in the horizontal and vertical direction and a light stone background for the content to sit on.

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -52,7 +52,7 @@ function Order() {
         </p>
       </div>
 
-      <div>
+      <div className="space-y-2 bg-stone-200 px-6 py-5">
         <p>Price pizza: {formatCurrency(orderPrice)}</p>
         {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
         <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
