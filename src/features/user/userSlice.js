3c971eb2a2f3fec0a928ca78bb2d7af0d99211b9
MBANS8A1 <seanmbng@aol.com>
2026-01-26 23:29:26 +0000

    I commented out the address geolocation code for now in useSlice.js, as I will not need it yet.

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,3 +1,6 @@
+import { getAddress } from '../../services/apiGeocoding';
+
+/*
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -19,3 +22,4 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+  */
