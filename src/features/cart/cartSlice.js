249fbfe8a41ab523ca230ff86b5214f795169c3a
MBANS8A1 <seanmbng@aol.com>
2026-01-28 20:28:33 +0000

    For the action creators generated from the cartSlice, I have added the logic for the five of them (to add, remove, increase the quantity, decrease the quanitity and to clear the cart). I then provided named exports for the five action creators and the reducer itself.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,10 +18,40 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItem(state, action) {},
-    deleteItem(state, action) {},
-    increaseItemQuantity(state, action) {},
-    decreaseItemQuantity(state, action) {},
-    clearCart(state, action) {},
+    //payload: a new item object
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload: will be the id of the item object (pizzaId)
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      //payload: will be the id of the item object (pizzaId)
+
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      //payload: will be the id of the item object (pizzaId)
+
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item.quantity > 0) item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
   },
 });
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
