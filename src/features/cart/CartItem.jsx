f5bff5cc466adfc7b4cf29572dd49efb52992c82
MBANS8A1 <seanmbng@aol.com>
2026-01-29 19:57:10 +0000

    I made some adjustments to the CartItems alignment by adding some additional Tailwind styling.

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -6,11 +6,13 @@ function CartItem({ item }) {
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-center">
-      <p className="mb-1 sm:mb-0">
+      <p className="mb-1 sm:mr-2 sm:mb-0">
         {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
-        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <p className="text-sm font-bold sm:mt-0.5">
+          {formatCurrency(totalPrice)}
+        </p>
         <Button type="small">Delete</Button>
       </div>
     </li>
