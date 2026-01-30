ad28d69018f06ff88adb461d00a34da69c73f672
MBANS8A1 <seanmbng@aol.com>
2026-01-30 16:00:05 +0000

    I added the onClick prop to the Button components in the returned JSX. They contain arrow functions, which will dispatch action creators increaseItemQuantity and decreaseItenQuantity with the pizzaId being the payload.

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,23 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
 function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
   return (
     <div className="flex items-center gap-1 md:gap-3">
-      <Button type="round">+</Button>
-      <Button type="round">-</Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
     </div>
   );
 }
