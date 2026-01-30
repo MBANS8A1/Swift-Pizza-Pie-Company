09e90243a0f7d850ed50df707ab7729d96b737ba
MBANS8A1 <seanmbng@aol.com>
2026-01-30 17:44:03 +0000

    Accompanying the Buttons for increasing/decreasing the item quantity, I want the current amount of each pizza to be displayed in the middle of the Buttons. To do this, I took the currentQuantity selector function (getCurrentQuantityById), which is in the MenuItem component and copied it into the CartItem component. I then passed down the currentQuantity variable into the UpdateItemQuantity component instance. In the  UpdateItemQuantity component, I used the currentQuantity prop in a <span> element between the "+" and "-" Button components. The inline <span> element has smaller text and a font-weight of 500. I made the gap in the <div> container larger to accommodate the <span> element better.

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
 import DeleteItem from './DeleteItem';
 import UpdateItemQuantity from './UpdateItemQuantity';
+import { getCurrentQuantityById } from './cartSlice';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-center">
@@ -14,7 +18,10 @@ function CartItem({ item }) {
         <p className="text-sm font-bold sm:mt-0.5">
           {formatCurrency(totalPrice)}
         </p>
-        <UpdateItemQuantity pizzaId={pizzaId} />
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -39,7 +39,6 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       if (item.quantity > 0) item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
-
       if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
