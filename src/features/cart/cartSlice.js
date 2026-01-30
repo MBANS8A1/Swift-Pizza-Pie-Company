0087a3eef0af8389fbf96b436abe24ae2ee19549
MBANS8A1 <seanmbng@aol.com>
2026-01-30 11:52:46 +0000

    I need the quantity of each individual pizza item to control whether the DeleteItem Button should be shown for a MenuItem or not. I created a selector function called getCurrentQuantityById, which uses the pizza item's id. In cartSlice.js, I exported the selector function, which checks if the pizza's pizzaId matches the id from the parameter; if it does it will get the quantity after optional chaining, otherwise it will return 0 (due to the action of he nullish coalescing operator).

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -63,3 +63,6 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((acc, currItem) => acc + currItem.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { addItem } from '../cart/cartSlice';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
 
   function handleAddToCart() {
     const newItem = {
