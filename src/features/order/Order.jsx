47fc557af197f3c3fa06376e03529250a78c2f73
MBANS8A1 <seanmbng@aol.com>
2026-02-02 23:24:14 +0000

    I want to load in the menu (ingredients) data associated with an ordered pizza item into the Order component. I am using the useFetcher hook with a useEffect hook; so when the Order page loads the menu data from the "/menu" route will be loaded into the Order (via the fetcher) component without needed navigation.

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
@@ -8,11 +8,16 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import OrderItem from './OrderItem';
+import { useEffect } from 'react';
 
 function Order() {
   const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  const fetcher = useFetcher();
 
+  useEffect(function () {
+    fetcher.load('/menu');
+  }, []);
   const {
     id,
     status,
