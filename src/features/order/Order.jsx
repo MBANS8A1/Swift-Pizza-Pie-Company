3dcd86b8342ecc8993cd2baa863440997d07e4fe
MBANS8A1 <seanmbng@aol.com>
2026-02-03 10:39:55 +0000

    In addition, I added an if guard statement within the useEeffect hook. I only want the data to be loaded in from the "/menu" page if there is no data loaded in. So, I added the modification on line 20 in Order.jsx.

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -15,9 +15,12 @@ function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const fetcher = useFetcher();
 
-  useEffect(function () {
-    fetcher.load('/menu');
-  }, []);
+  useEffect(
+    function () {
+      if (!fetcher.data) fetcher.load('/menu');
+    },
+    [fetcher],
+  );
   const {
     id,
     status,
